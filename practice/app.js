5a644985e80742be072dcec2ff1f965f8e325fca
seirenaa <seiren0903@gmail.com>
2026-03-19 18:29:21 +0900

    add:questinの解答

---
+++ practice/app.js
@@ -0,0 +1,46 @@
+
+console.log('Hello World!');
+console.log('1つ目の値', '2つ目の値');
+
+let text = 'JavaScriptの練習';
+console.log(text);
+
+text = 'JavaScriptをマスターした';
+console.log(text);
+
+
+let longText =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+console.log(longText);
+console.log(longText);
+console.log(longText);
+
+// 処理②
+console.log(
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+);
+console.log(
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+);
+console.log(
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+);
+
+let testText;
+console.log(testText)
+
+let again = '定義しました';
+again = '再代入はできます';
+
+console.log(again);
+
+console.log('elephant'); // => elephant が出力される
+
+let name = 'ジョニー';
+let greet = '私は' + name + '！';
+console.log(greet);
+
+let template = 'テンプレートリテラル'; // ※ これは文字列
+let templateText = `これが${template}です。
+${template}は改行もできます。`; // これはテンプレートリテラル
+console.log(templateText);
